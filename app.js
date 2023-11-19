ef5fa6e964bfb2138be5bdee880d3e366127b66d
Jay19981112 <jeifujinami1112@gmail.com>
2023-11-19 15:46:56 +0900

    completed styles

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-$("#addTodo").click(function (){
+$("#addTodo").click(function () {
     const inputTodo = $("input").val();
-    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+    const listItem = $("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    listItem.css({
+        "opacity": "0",
+        "transform": "translateY(10px)",
+    });
+
+    $("#todoList").append(listItem);
+    listItem.animate({
+        "opacity": "1",
+    }, 500);
 
     $("input").val("");
+});
+
+
+
+$(document).on("change", "input[type=checkbox]", function() {
+
+    if($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "#000");
+    }
 });
